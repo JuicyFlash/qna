d1410688ac529f6b4a609ebb7ea108c38ebea7ae
JuicyFlash <tankedet@bk.ru>
2023-01-19 15:57:03 +0300

    Добавлены тесты для метода модели Question Рефакторинг views/rewards/_reward

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -14,4 +14,50 @@ RSpec.describe Question, type: :model do
   it 'have many attached file' do
     expect(Question.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
   end
+  describe 'reward best answer' do
+    let!(:user) { create(:user) }
+    let!(:question) { create(:question, author: user, best_answer: nil) }
+    let!(:answer) { create(:answer, author: user, question: question) }
+    let!(:reward) { create(:reward, question: question) }
+
+    it 'set reference from question`s reward to the best answer`s author' do
+      question.best_answer = answer
+
+      expect(user.rewards.first).to eq nil
+
+      question.reward_best_answer
+      question.save
+
+      expect(user.rewards.first).to eq reward
+    end
+    it 'change rewarded user, after change best answer' do
+      other_user = create(:user)
+      other_answer = create(:answer, author: other_user, question: question)
+      question.best_answer = answer
+      question.reward_best_answer
+      question.save
+
+      expect(user.rewards.first).to eq reward
+
+      question.best_answer = other_answer
+      question.reward_best_answer
+      question.save
+
+      expect(user.rewards.first).to eq nil
+      expect(other_user.rewards.first).to eq reward
+    end
+    it 'remove reference from question`s reward to the answer`s author, if attribute best was removed' do
+      question.best_answer = answer
+      question.reward_best_answer
+      question.save
+
+      expect(user.rewards.first).to eq reward
+
+      question.best_answer_id = nil
+      question.reward_best_answer
+      question.save
+
+      expect(user.rewards.first).to eq nil
+    end
+  end
 end
